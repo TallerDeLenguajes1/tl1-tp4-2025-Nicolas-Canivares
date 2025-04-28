b12bdf14dd8e55d440db5b1b5ccf2764772da3d6
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-04-28 19:36:59 -0300

    Punto 2 del tp

--- punto2.c
+++ punto2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>   
 #include <stdlib.h>
 
+int idIncremental = 1000; //Variable global para el ID de la tarea
+
 struct{
     int TareaID;//Numérico autoincremental comenzando en 1000
     char *Descripcion; //
@@ -8,14 +10,82 @@ struct{
 } typedef Tarea;
 
 struct{
-    Tarea T;
-    Nodo *Siguiente;
+    Tarea Tarea; //Dato
+    Nodo *Siguiente; //Puntero al siguiente nodo
 } typedef Nodo;
+
+void crearTarea(Tarea *tareas);
+
+Nodo * CrearListaVacia();
+
+Nodo * CrearNodo(Tarea nuevaTarea);
+
 
 
 int main()
 {
+    srand(time(NULL)); // Inicializa la semilla para la función rand()
+    Nodo *listaTareasPendientes;
+    // Crear la lista vacía de tareas pendientes
+    listaTareasPendientes = CrearListaVacia();
+
+    int opcion, seguir = 1;
+    Tarea nuevaTarea;
+    
+    
+
+    //MENU
+    do
+    {
+
+        printf("1. Crear tarea pendiente\n");
+        printf("Ingrese la opcion deseada:\n");
+        scanf("%d", &opcion);
+        if (opcion == 1)
+        {   
+            while (seguir != 0)
+            {
+                crearTarea(&nuevaTarea);
+                printf("Desea crear otra tarea? (1: Si, 0: No)\n");
+                scanf("%d", &seguir);
+                Nodo *NuevoNodo = CrearNodo(nuevaTarea);
+
+            }
+            
+        }
+        
+    } while (opcion != 0);
+    
+
 
 
     return 0;
+}
+
+void crearTarea(Tarea *tareas){
+    tareas->TareaID = idIncremental;
+    printf("Ingrese la descripcion de la tarea:\n");
+    char *descripcion = (char *) malloc(100 * sizeof(char));
+    gets(descripcion);
+    fflush(stdin);
+    int tamanioDescripcion = strlen(descripcion);
+    tareas->Descripcion = (char *) malloc((tamanioDescripcion + 1) * sizeof(char));
+    strcpy(tareas->Descripcion, descripcion);
+    free(descripcion); // Liberar memoria de la variable temporal
+    tareas->Duracion = rand() % 91 + 10; // Genera un número aleatorio entre 10 y 100
+    idIncremental++;
+    printf("Tarea creada!\n");
+}
+
+Nodo * CrearNodo(Tarea nuevaTarea)
+{
+    Nodo * nuevoNodo = (Nodo *) malloc (sizeof(Nodo));
+    nuevoNodo->Tarea = nuevaTarea;
+    nuevoNodo->Siguiente = NULL;
+    return nuevoNodo;
+}
+
+Nodo * CrearListaVacia()
+{
+    return NULL;
 }
